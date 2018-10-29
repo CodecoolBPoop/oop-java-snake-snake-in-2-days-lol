d715817421861908ad203d3346a0a25afae6436c
Jani <janimarton22@gmail.com>
2018-10-29 18:05:19 +0100

    multiplayer mode implemented

--- src/com/codecool/snake/Game.java
+++ src/com/codecool/snake/Game.java
@@ -7,11 +7,17 @@ import com.codecool.snake.eventhandler.InputHandler;
 
 import com.sun.javafx.geom.Vec2d;
 import javafx.scene.Scene;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.Pane;
+import javafx.scene.control.Alert;
+
+import java.util.Optional;
 
 
 public class Game extends Pane {
     private Snake snake = null;
+    private Snake snake2 = null;
     private GameTimer gameTimer = new GameTimer();
 
 
@@ -24,11 +30,21 @@ public class Game extends Pane {
     }
 
     public void init() {
+
+        // if 2 player mode active: snake2 exist
+
+        boolean singlePlayer = startingChoosePlayerNumbers();
+
         spawnSnake();
+
+        if (!singlePlayer) {
+            spawnSnake2();
+        }
+
         spawnEnemies(4);
         spawnPowerUps(4);
 
-        GameLoop gameLoop = new GameLoop(snake);
+        GameLoop gameLoop = new GameLoop(snake, snake2);
         Globals.getInstance().setGameLoop(gameLoop);
         gameTimer.setup(gameLoop::step);
         gameTimer.play();
@@ -43,17 +59,48 @@ public class Game extends Pane {
         snake = new Snake(new Vec2d(500, 500));
     }
 
+    private void spawnSnake2() {
+        snake2 = new Snake(new Vec2d(400, 400));
+        snake2.secondSnakeDiretionSetter();
+    }
+
     private void spawnEnemies(int numberOfEnemies) {
-        for(int i = 0; i < numberOfEnemies; ++i) new SimpleEnemy();
+        for (int i = 0; i < numberOfEnemies; ++i) new SimpleEnemy();
     }
 
     private void spawnPowerUps(int numberOfPowerUps) {
-        for(int i = 0; i < numberOfPowerUps; ++i) new SimplePowerUp();
+        for (int i = 0; i < numberOfPowerUps; ++i) new SimplePowerUp();
     }
 
     private void setupInputHandling() {
         Scene scene = getScene();
         scene.setOnKeyPressed(event -> InputHandler.getInstance().setKeyPressed(event.getCode()));
         scene.setOnKeyReleased(event -> InputHandler.getInstance().setKeyReleased(event.getCode()));
+    }
+
+    private boolean startingChoosePlayerNumbers() {
+
+        boolean playAsSingle = true;
+
+        Alert choosePlayerNumbers = new Alert(Alert.AlertType.CONFIRMATION);
+        choosePlayerNumbers.setTitle("Welcome in the Snake in 2 days lol!");
+        choosePlayerNumbers.setHeaderText("Please choose between SinglePlayer and MultiPlayer");
+
+
+        ButtonType buttonTypeCancelPerSinglePlayer = new ButtonType("Single Player", ButtonBar.ButtonData.CANCEL_CLOSE);
+        ButtonType multiPlayerButton = new ButtonType("Multi Player");
+
+        choosePlayerNumbers.getButtonTypes().setAll(buttonTypeCancelPerSinglePlayer, multiPlayerButton);
+
+
+        Optional<ButtonType> result = choosePlayerNumbers.showAndWait();
+        if (result.get() == buttonTypeCancelPerSinglePlayer) {
+            return playAsSingle;
+        } else if (result.get() == multiPlayerButton) {
+            playAsSingle = false;
+            return playAsSingle;
+        } else {
+            return playAsSingle;
+        }
     }
 }
