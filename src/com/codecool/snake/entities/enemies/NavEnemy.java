eb4ce843eefaf1204650180c53f187e2e9ca1481
Noemi Szoke <szokenoemijob@gmail.com>
2018-10-30 14:44:15 +0100

    Nav Enemy is working

---
+++ src/com/codecool/snake/entities/enemies/NavEnemy.java
@@ -0,0 +1,54 @@
+package com.codecool.snake.entities.enemies;
+
+import com.codecool.snake.entities.GameEntity;
+import com.codecool.snake.Globals;
+import com.codecool.snake.entities.Animatable;
+import com.codecool.snake.Utils;
+import com.codecool.snake.entities.Interactable;
+import com.codecool.snake.entities.snakes.SnakeHead;
+import java.util.Random;
+
+import javafx.geometry.Point2D;
+
+public class NavEnemy extends Enemy implements Animatable, Interactable {
+
+    private Point2D heading;
+    private static Random rnd = new Random();
+
+    public NavEnemy() {
+        super(10);
+
+        setImage(Globals.getInstance().getImage("NavEnemy"));
+        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
+        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
+
+        double direction = rnd.nextDouble() * 360;
+        setRotate(direction);
+
+        int speed = 1;
+        heading = Utils.directionToVector(direction, speed);
+    }
+
+    @Override
+    public void step() {
+        if (isOutOfBounds()) {
+            destroy();
+            new NavEnemy();
+        }
+        setX(getX() + heading.getX());
+        setY(getY() + heading.getY());
+    }
+
+    @Override
+    public void apply(GameEntity entity) {
+        if(entity instanceof SnakeHead){
+            System.out.println(getMessage());
+            destroy();
+        }
+    }
+
+    @Override
+    public String getMessage() {
+        return (getDamage() + " damage");
+    }
+}
