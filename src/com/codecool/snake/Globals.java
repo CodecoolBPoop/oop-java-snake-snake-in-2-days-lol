08b423ed8300ed22e45f67de9ee07f78a0d7a8e3
Noemi Szoke <szokenoemijob@gmail.com>
2018-10-30 11:18:01 +0100

    new pictures added

--- src/com/codecool/snake/Globals.java
+++ src/com/codecool/snake/Globals.java
@@ -33,6 +33,9 @@ public class Globals {
         resources.addImage("SnakeBody", new Image("snake_body.png"));
         resources.addImage("SimpleEnemy", new Image("simple_enemy.png"));
         resources.addImage("PowerUpBerry", new Image("powerup_berry.png"));
+        resources.addImage("PoliceCap", new Image("police_hat.png"));
+        resources.addImage("NAV", new Image("NAV_great.png"));
+        resources.addImage("Background", new Image("Snake_new_island_background.jpg"));
     }
 
     public Image getImage(String name) { return resources.getImage(name); }
